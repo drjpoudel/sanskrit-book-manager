b8b069b00563ed5f6c218078b2c9d89ace69f5d8
drjpoudel <drjpoudel@gmail.com>
2025-08-28 13:44:31 +0545

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php // header.php ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php // footer.php ?>
+<?php wp_footer(); ?>
+</body>
+</html>
